0c8905871694657d42f7d29122f06ee71a9e844a
seed <ddhruvgupta@gmail.com>
2019-11-07 20:35:05 -0500

    readme2.c created

--- readme.c
+++ readme.c
@@ -25,54 +25,39 @@ if(flag == 0){
 	FILE *fp2 = fopen("readme", "r");
  
 	char cbuffer [100000];
+	char cbuffer_copy [100000];
 	char bbuffer [20000];
 	int total = fread(cbuffer, 1, sizeof(cbuffer), fp);
 	int total2 = fread(bbuffer, 1, sizeof(bbuffer), fp2);
 
 	//search for string in bbuffer
-	int i;
-	for(i = 0; i < total2; i++ ){
-		if(bbuffer[i] == 'h' 
-			&& bbuffer[i+1] == 'e'	
-			&& bbuffer[i+2] == 'l'
-			&& bbuffer[i+3] == 'l' 
-			&& bbuffer[i+4] == 'o'){
-			printf("value of i = %d\n",i );
-			break;
-		}
-	}
+	int i, j = 0;
+		for(i = 0; i < total; i++ )
+		{
+			cbuffer_copy[i] = cbuffer[i];
 
-	// replace the contents in bbuffer with text from cbuffer
-	int j = 0;
-	for(j; j < total; j++, i++ ){
-		//printf("%c\n", bbuffer[i]);
-		bbuffer[i] = cbuffer[j];
-		
-		// if (cbuffer[j] == EOF)
-		// 	break;
-		
-	}
+			if(cbuffer[i] == 'h' && cbuffer[i+1] == 'e'	&& cbuffer[i+2] == 'l'&& cbuffer[i+3] == 'l' && cbuffer[i+4] == 'o')
+			{
+				printf("Adding \n " );
+				for( j=0; j<total2; j++)
+					cbuffer_copy[i+j] = cbuffer[j];
+				break;
+			}
+		}
+
+		for(i = i+5; i < total; i++)
+		{
+			cbuffer_copy[i+j] = cbuffer[i];
+		}
+	
+	
 	fclose(fp);
 	fclose(fp2);
-// if run = 2, write contents of a to new .c file
-
-	for(int k = 0; k< sizeof(a)/sizeof(a[0]);k++ )
-		printf("%c", a[k]);
 
 	FILE *fp_readme2 = fopen("readme2.c", "w");
-	fwrite (a , sizeof(char), sizeof(a), fp_readme2);
+	fwrite (cbuffer_copy , sizeof(char), sizeof(cbuffer_copy), fp_readme2);
 	fclose(fp_readme2);
-	//char command[50];
-    //strcpy( command, "gcc readme2.c -o readme2" );
-	//system(command);
-
-}else{
-
-}
-
-
-
-// compile .c file
+	}
 
 	return 0;
 }
